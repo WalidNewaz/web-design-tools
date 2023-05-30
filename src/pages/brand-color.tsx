53cacf57b49f96ebeecf43942d26a790a6f09bee
Walid Newaz <mdwalid@gmail.com>
2023-05-30 16:49:13 -0600

    Added pages to experiment with brand color.

---
+++ src/pages/brand-color.tsx
@@ -0,0 +1,111 @@
+import * as React from "react"
+import type { HeadFC, PageProps } from "gatsby"
+import { Link } from "gatsby"
+import "../style.css"
+
+const BrandColor: React.FC<PageProps> = () => {
+    return (
+        <>
+            <header>
+                <section>
+                    <ul className="main-menu">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/brand-color">Color Scheme</Link></li>
+                    </ul>
+                </section>
+            </header>
+            <main>
+                <h1>Brand Color</h1>
+                <h2>Light Theme</h2>
+                <section className="light-theme">
+                    <figure className="brand-light">
+                        <div className="swatch"></div>
+                        <figcaption>Brand</figcaption>
+                    </figure>
+
+                    <figure className="text1-light">
+                        <div className="swatch"></div>
+                        <figcaption>Text 1</figcaption>
+                    </figure>
+
+                    <figure className="text2-light">
+                        <div className="swatch"></div>
+                        <figcaption>Text 2</figcaption>
+                    </figure>
+
+                    <figure className="surface1-light">
+                        <div className="swatch"></div>
+                        <figcaption>Surface 1</figcaption>
+                    </figure>
+
+                    <figure className="surface2-light">
+                        <div className="swatch"></div>
+                        <figcaption>Surface 2</figcaption>
+                    </figure>
+
+                    <figure className="surface3-light">
+                        <div className="swatch"></div>
+                        <figcaption>Surface 3</figcaption>
+                    </figure>
+
+                    <figure className="surface4-light">
+                        <div className="swatch"></div>
+                        <figcaption>Surface 4</figcaption>
+                    </figure>
+
+                    <figure className="shadow-light">
+                        <div className="swatch"></div>
+                        <figcaption>Shadow</figcaption>
+                    </figure>
+                </section>
+
+                <h2>Dark Theme</h2>
+                <section className="dark-theme">
+                    <figure className="brand">
+                        <div className="swatch"></div>
+                        <figcaption>Brand</figcaption>
+                    </figure>
+
+                    <figure className="text1">
+                        <div className="swatch"></div>
+                        <figcaption>Text 1</figcaption>
+                    </figure>
+
+                    <figure className="text2">
+                        <div className="swatch"></div>
+                        <figcaption>Text 2</figcaption>
+                    </figure>
+
+                    <figure className="surface1">
+                        <div className="swatch"></div>
+                        <figcaption>Surface 1</figcaption>
+                    </figure>
+
+                    <figure className="surface2">
+                        <div className="swatch"></div>
+                        <figcaption>Surface 2</figcaption>
+                    </figure>
+
+                    <figure className="surface3">
+                        <div className="swatch"></div>
+                        <figcaption>Surface 3</figcaption>
+                    </figure>
+
+                    <figure className="surface4">
+                        <div className="swatch"></div>
+                        <figcaption>Surface 4</figcaption>
+                    </figure>
+
+                    <figure className="shadow">
+                        <div className="swatch"></div>
+                        <figcaption>Shadow</figcaption>
+                    </figure>
+                </section>
+            </main>
+        </>
+    )
+}
+
+export default BrandColor
+
+export const Head: HeadFC = () => <title>Brand color picker</title>
