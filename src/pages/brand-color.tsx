f2eec5e46fa2aba9e3460ed3facfbda0be849bc6
Walid Newaz <mdwalid@gmail.com>
2023-05-30 19:26:44 -0600

    Added color-scheme attribute driven CSS

--- src/pages/brand-color.tsx
+++ src/pages/brand-color.tsx
@@ -18,42 +18,42 @@ const BrandColor: React.FC<PageProps> = () => {
                 <h1>Brand Color</h1>
                 <h2>Light Theme</h2>
                 <section className="light-theme">
-                    <figure className="brand-light">
+                    <figure className="brand">
                         <div className="swatch"></div>
                         <figcaption>Brand</figcaption>
                     </figure>
 
-                    <figure className="text1-light">
+                    <figure className="text1">
                         <div className="swatch"></div>
                         <figcaption>Text 1</figcaption>
                     </figure>
 
-                    <figure className="text2-light">
+                    <figure className="text2">
                         <div className="swatch"></div>
                         <figcaption>Text 2</figcaption>
                     </figure>
 
-                    <figure className="surface1-light">
+                    <figure className="surface1">
                         <div className="swatch"></div>
                         <figcaption>Surface 1</figcaption>
                     </figure>
 
-                    <figure className="surface2-light">
+                    <figure className="surface2">
                         <div className="swatch"></div>
                         <figcaption>Surface 2</figcaption>
                     </figure>
 
-                    <figure className="surface3-light">
+                    <figure className="surface3">
                         <div className="swatch"></div>
                         <figcaption>Surface 3</figcaption>
                     </figure>
 
-                    <figure className="surface4-light">
+                    <figure className="surface4">
                         <div className="swatch"></div>
                         <figcaption>Surface 4</figcaption>
                     </figure>
 
-                    <figure className="shadow-light">
+                    <figure className="shadow">
                         <div className="swatch"></div>
                         <figcaption>Shadow</figcaption>
                     </figure>
@@ -61,6 +61,49 @@ const BrandColor: React.FC<PageProps> = () => {
 
                 <h2>Dark Theme</h2>
                 <section className="dark-theme">
+                    <figure className="brand">
+                        <div className="swatch"></div>
+                        <figcaption>Brand</figcaption>
+                    </figure>
+
+                    <figure className="text1">
+                        <div className="swatch"></div>
+                        <figcaption>Text 1</figcaption>
+                    </figure>
+
+                    <figure className="text2">
+                        <div className="swatch"></div>
+                        <figcaption>Text 2</figcaption>
+                    </figure>
+
+                    <figure className="surface1">
+                        <div className="swatch"></div>
+                        <figcaption>Surface 1</figcaption>
+                    </figure>
+
+                    <figure className="surface2">
+                        <div className="swatch"></div>
+                        <figcaption>Surface 2</figcaption>
+                    </figure>
+
+                    <figure className="surface3">
+                        <div className="swatch"></div>
+                        <figcaption>Surface 3</figcaption>
+                    </figure>
+
+                    <figure className="surface4">
+                        <div className="swatch"></div>
+                        <figcaption>Surface 4</figcaption>
+                    </figure>
+
+                    <figure className="shadow">
+                        <div className="swatch"></div>
+                        <figcaption>Shadow</figcaption>
+                    </figure>
+                </section>
+
+                <h2>Dim Theme</h2>
+                <section className="dim-theme">
                     <figure className="brand">
                         <div className="swatch"></div>
                         <figcaption>Brand</figcaption>
